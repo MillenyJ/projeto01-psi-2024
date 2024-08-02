15db8ce556d2bc729d75b47ded247fa4aeb0b137
Helloar <novaeheloar@gmail.com>
2024-08-02 09:01:42 -0300

    base do site

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def index(request):
+    return render(request, "index.html")
+
+def atletas(request):
+    return render(request, "index.html")
+
+def sobre(request):
+    return render(request, "index.html")
