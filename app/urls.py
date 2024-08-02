13f631aede08b7b91f3ce0cf686f82d71e316d78
Milleny <153157418+MillenyJ@users.noreply.github.com>
2024-08-02 09:45:48 -0300

    Delete app directory

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name= "index"),
-    path('atletas/', views.atletas, name= "atletas"),
-    path('sobre/', views.sobre, name= "sobre"),
-]
